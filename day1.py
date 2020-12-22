0946abaf4c0953ee0aac5c745bbdf8803d9ffa55
Mesude <61362079+mesudepolat@users.noreply.github.com>
2020-12-22 14:18:17 +0300

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,19 @@
+y = int(input("Please enter an integer: "))
+x = float(input("Please enter a float: "))
+z = str(input("Please enter a string"))
+m = "value4"
+input(f'{m}')
+k = "value5"
+input(f'{k}')
+
+print(y)
+print(type(y))
+print(x)
+print(type(x))
+print(z)
+print(type(z))
+print(m)
+print(type("value4"))
+print(k)
+print(type("value5"))
+
